8827576853fdbbaf28d7a5ca912b46202830e46f
unknown <adrianmihaicater1234@gmail.com>
2026-07-11 18:23:55 +0300

    Updated AttendeeMapper

--- src/main/java/com/autominutes/backend/mapper/AttendeeMapper.java
+++ src/main/java/com/autominutes/backend/mapper/AttendeeMapper.java
@@ -1,10 +1,24 @@
 package com.autominutes.backend.mapper;
 
+import com.autominutes.backend.dto.AttendeeCreateRequest;
 import com.autominutes.backend.dto.AttendeeDTO;
+import com.autominutes.backend.dto.AttendeeUpdateRequest;
 import com.autominutes.backend.entity.Attendee;
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = "spring")
 public interface AttendeeMapper {
+
     AttendeeDTO toDto(Attendee attendee);
+
+    Attendee toEntity(AttendeeCreateRequest request);
+
+    // la fel ca la Meeting: daca un camp e null in request, nu suprascrie valoarea existenta
+    @Mapping(target = "id", ignore = true)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    void updateEntityFromRequest(AttendeeUpdateRequest request, @MappingTarget Attendee attendee);
 }
