3c7b6abd8eae0877fe2ef4b2fb0a175fc53616df
unknown <adrianmihaicater1234@gmail.com>
2026-07-11 19:13:02 +0300

    Added name as optional for put

--- src/main/java/com/autominutes/backend/dto/AttendeeUpdateRequest.java
+++ src/main/java/com/autominutes/backend/dto/AttendeeUpdateRequest.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record AttendeeUpdateRequest(
-        @NotBlank(message = "Name cannot be blank")
+
         String name,
 
         @Email(message = "Email is not valid")
